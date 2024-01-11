2ad287b38f5b44a7da131ab5c5ed168a5ed2d931
PiyushDatta <piyushdattaca@gmail.com>
2024-01-10 23:24:51 -0500

    This probably fixes the path for the resume pdf.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -34,11 +34,7 @@ const Resume = () => (
         <div className="title">
           <h2>
             <Link to="resume">Resume</Link>&nbsp;&nbsp;
-            <a
-              href={`${process.env.PUBLIC_URL}${'/resumes/piyush_datta_resume.pdf'}`}
-            >
-              (PDF Version)
-            </a>
+            <a href="/resumes/piyush_datta_resume.pdf">(PDF Version)</a>
           </h2>
           <div className="link-container">
             {Object.keys(sections).map((sec) => (
