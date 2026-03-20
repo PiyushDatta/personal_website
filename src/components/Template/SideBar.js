bc30dc45721e3d500415f0fc30d0dfc17cbeac56
Piyush D <piyushdattaca@gmail.com>
2026-03-20 00:07:30 -0700

    edit about me

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -22,8 +22,8 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>
-        My name is Piyush (Pee-u-sh). I&apos;m a self-taught systems software
-        engineer currently living and working in SF Bay Area. I&apos;ve worked on
+        My name is Piyush (Pee-u-sh). I&apos;m a software
+        engineer currently living and working in San Francisco. I&apos;ve worked on
         massive scale projects within database kernels and high performance
         infrastructure systems. My interests lie in distributed systems, ML/AI
         (main focus on sparse LLM architectures and RL), and cloud computing.
